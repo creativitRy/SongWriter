ee25cbb359ec7196d82de33e9dfdae476c8deee4
creativitRy <creativitRy@gmail.com>
2016-01-10 14:23:46 -0600

    created a method in SongWriter.java to easily choose a random note

--- src/ChordStack.java
+++ src/ChordStack.java
@@ -10,272 +10,272 @@ import java.util.*;
 
 public class ChordStack
 {
-    private int root; //0=tonic, 6=subtonic/leading tone
-    private boolean minor; //true=minor root, false=major root
-    private boolean seventh; //true if chord is 7th, false if chord is triad
-    private int inversion; //0=none, 1=1st, 2=2nd, 3=3rd
-    
-    public ChordStack()
-    {
-    	root = 0;
-    	minor = false;
-    	seventh = false;
-    	inversion = 0;
-    }
-    public ChordStack(int rootGiven, boolean isMinorGiven)
-    {
-    	root = rootGiven;
-    	minor = isMinorGiven;
-    	seventh = false;
-    	inversion = 0;
-    }
-    public ChordStack(int rootGiven, boolean isMinorGiven, boolean isSeventhGiven)
-    {
-    	root = rootGiven;
-    	minor = isMinorGiven;
-    	seventh = isSeventhGiven;
-    	inversion = 0;
-    }
-    public ChordStack(int rootGiven, boolean isMinorGiven, int invGiven)
-    {
-    	root = rootGiven;
-    	minor = isMinorGiven;
-    	seventh = (invGiven>2?true:false);
-    	inversion = invGiven;
-    }
-    public ChordStack(int rootGiven, boolean isMinorGiven, boolean isSeventhGiven, int invGiven)
-    {
-    	root = rootGiven;
-    	minor = isMinorGiven;
-    	seventh = isSeventhGiven;
-    	inversion = invGiven;
-    }
-    
-    public ChordStack(String romanNum, boolean isMinorGiven)
-    {
-    	minor = isMinorGiven;
-    	
-    	if (romanNum.toLowerCase().indexOf("vii") != -1)
-    		root = 6;
-    	else if (romanNum.toLowerCase().indexOf("vi") != -1)
-    		root = 5;
-    	else if (romanNum.toLowerCase().indexOf("iv") != -1)
-    		root = 3;
-    	else if (romanNum.toLowerCase().indexOf("v") != -1)
-    		root = 4;
-    	else if (romanNum.toLowerCase().indexOf("iii") != -1)
-    		root = 2;
-    	else if (romanNum.toLowerCase().indexOf("ii") != -1)
-    		root = 1;
-    	else// if (romanNum.toLowerCase().indexOf("i") != -1)
-    		root = 0;
-    		
-    	if (romanNum.indexOf("42") != -1)
-    	{
-    		inversion = 3;
-    		seventh = true;
-    	}
-    	else if (romanNum.indexOf("43") != -1)
-    	{
-    		inversion = 2;
-    		seventh = true;
-    	}
-    	else if (romanNum.indexOf("65") != -1)
-    	{
-    		inversion = 1;
-    		seventh = true;
-    	}
-    	else if (romanNum.indexOf("7") != -1)
-    	{
-    		inversion = 0;
-    		seventh = true;
-    	}
-    	else if (romanNum.indexOf("64") != -1)
-    		inversion = 2;
-    	else if (romanNum.indexOf("6") != -1)
-    		inversion = 1;
-    	else
-    		inversion = 0;
-    }
-    /* private int root; //0=tonic, 6=subtonic/leading tone
-    private boolean isMinor; //true=minor root, false=major root
-    private boolean isSeventh; //true if chord is 7th, false if chord is triad
-    private int inversion; //0=none, 1=1st, 2=2nd, 3=3rd
-     */
-    public int getRoot()
-    {
-    	return root;
-    }
-    
-    public boolean isMinor()
-    {
-    	return minor;
-    }
-    
-    public boolean isSeventh()
-    {
-    	return seventh;
-    }
-    
-    public int getInversion()
-    {
-    	return inversion;
-    }
-    
-    public String getFiguredBassInversion()
-    {
-    	if (seventh)
-    	{
-    		if (inversion == 0)
-    			return "7";
-    		else if (inversion == 1)
-    			return "65";
-    		else if (inversion == 2)
-    			return "43";
-    		else
-    			return "42";
-    	}
-    	else
-    	{
-    		if (inversion == 0)
-    			return "";
-    		else if (inversion == 1)
-    			return "6";
-    		else
-    			return "64";
-    	}
-    }
-    /**
-     * Identifies the intervals for each scale degree
-     * array[num] is between num note and (num+1)%7 note
-     * @return Array of steps for each scale degree starting from tonic note. 
-     */
-    public int[] getIntervals()
-    {
-    	int[] majorIntervals = {2,2,1,2,2,2,1};
-    	int[] minorIntervals = {2,1,2,2,1,3,1};
-    	
-    	if (minor)
-    		return minorIntervals;
-    	return majorIntervals;
-    }
-    
-    public int getNote(int noteNum)
-    {
-    	int note = 0;
-    	int[] intervals = getIntervals();
-    		
-    	for (int i = 0; i < (noteNum % 12); i++)
-    	{
-    		note = (note + intervals[i]) % 12;
-    	}
-    	
-    	return note;
-    }
-    public int getNote(int noteNum, int tonic)
-    {
-    	int note = tonic;
-    	int[] intervals = getIntervals();
-    		
-    	for (int i = 0; i < (noteNum % 12); i++)
-    	{
-    		note = (note + intervals[i]) % 12;
-    	}
-    	
-    	return note;
-    }
-    
-    public int getBassNote()
-    {
-    	int bassNote = getNote(root + inversion * 2);
-    	
-    	return bassNote;
-    }
-    public int getBassNote(int tonic)
-    {
-    	int bassNote = getNote(root + inversion * 2, tonic);
-    	
-    	return bassNote;
-    }
-    
-    /**
-     *  returns all notes in an array
-     *  [0] = root
-     */
-    public int[] getAllNotes()
-    {
-    	int notes[] = new int[(seventh?4:3)];
-    	
-    	notes[0] = getNote(root);
-    	notes[1] = getNote(root + 2);
-    	notes[2] = getNote(root + 4);
-    	if (seventh)
-    		notes[3] = getNote(root + 6);
-    	
-    	return notes;
-    }
-    public int[] getAllNotes(int tonic)
-    {
-    	int notes[] = new int[(seventh?4:3)];
-    	
-    	notes[0] = getNote(root, tonic);
-    	notes[1] = getNote(root + 2, tonic);
-    	notes[2] = getNote(root + 4, tonic);
-    	if (seventh)
-    		notes[3] = getNote(root + 6, tonic);
-    	
-    	return notes;
-    }
-    /**
-     *  returns all notes in an array
-     *  [0] = bass if param is true
-     */
-    public int[] getAllNotes(boolean startFromBass)
-    {
-    	int notes[] = new int[(seventh?4:3)];
-    	
-    	notes[0] = getNote(root);
-    	notes[1] = getNote(root + 2);
-    	notes[2] = getNote(root + 4);
-    	if (seventh)
-    		notes[3] = getNote(root + 6);
-    	
-    	if (!startFromBass)
-    		return notes;
-    	
-    	int newNotes[] = new int[(seventh?4:3)];
-    	
-    	newNotes[0] = notes[inversion];
-    	newNotes[1] = notes[(inversion + 1) % (seventh?4:3)];
-    	newNotes[2] = notes[(inversion + 2) % (seventh?4:3)];
-    	if (seventh)
-    		newNotes[3] = notes[(inversion + 3) % (seventh?4:3)];
-    	
-    	return newNotes;
-    }
-    public int[] getAllNotes(boolean startFromBass, int tonic)
-    {
-    	int notes[] = new int[(seventh?4:3)];
-    	
-    	notes[0] = getNote(root, tonic);
-    	notes[1] = getNote(root + 2, tonic);
-    	notes[2] = getNote(root + 4, tonic);
-    	if (seventh)
-    		notes[3] = getNote(root + 6, tonic);
-    	
-    	if (!startFromBass)
-    		return notes;
-    	
-    	int newNotes[] = new int[(seventh?4:3)];
-    	
-    	newNotes[0] = notes[inversion];
-    	newNotes[1] = notes[(inversion + 1) % (seventh?4:3)];
-    	newNotes[2] = notes[(inversion + 2) % (seventh?4:3)];
-    	if (seventh)
-    		newNotes[3] = notes[(inversion + 3) % (seventh?4:3)];
-    	
-    	return newNotes;
-    }
+	private int root; //0=tonic, 6=subtonic/leading tone
+	private boolean minor; //true=minor root, false=major root
+	private boolean seventh; //true if chord is 7th, false if chord is triad
+	private int inversion; //0=none, 1=1st, 2=2nd, 3=3rd
+	
+	public ChordStack()
+	{
+		root = 0;
+		minor = false;
+		seventh = false;
+		inversion = 0;
+	}
+	public ChordStack(int rootGiven, boolean isMinorGiven)
+	{
+		root = rootGiven;
+		minor = isMinorGiven;
+		seventh = false;
+		inversion = 0;
+	}
+	public ChordStack(int rootGiven, boolean isMinorGiven, boolean isSeventhGiven)
+	{
+		root = rootGiven;
+		minor = isMinorGiven;
+		seventh = isSeventhGiven;
+		inversion = 0;
+	}
+	public ChordStack(int rootGiven, boolean isMinorGiven, int invGiven)
+	{
+		root = rootGiven;
+		minor = isMinorGiven;
+		seventh = (invGiven>2);
+		inversion = invGiven;
+	}
+	public ChordStack(int rootGiven, boolean isMinorGiven, boolean isSeventhGiven, int invGiven)
+	{
+		root = rootGiven;
+		minor = isMinorGiven;
+		seventh = isSeventhGiven;
+		inversion = invGiven;
+	}
+	
+	public ChordStack(String romanNum, boolean isMinorGiven)
+	{
+		minor = isMinorGiven;
+		
+		if (romanNum.toLowerCase().indexOf("vii") != -1)
+			root = 6;
+		else if (romanNum.toLowerCase().indexOf("vi") != -1)
+			root = 5;
+		else if (romanNum.toLowerCase().indexOf("iv") != -1)
+			root = 3;
+		else if (romanNum.toLowerCase().indexOf("v") != -1)
+			root = 4;
+		else if (romanNum.toLowerCase().indexOf("iii") != -1)
+			root = 2;
+		else if (romanNum.toLowerCase().indexOf("ii") != -1)
+			root = 1;
+		else// if (romanNum.toLowerCase().indexOf("i") != -1)
+			root = 0;
+			
+		if (romanNum.indexOf("42") != -1)
+		{
+			inversion = 3;
+			seventh = true;
+		}
+		else if (romanNum.indexOf("43") != -1)
+		{
+			inversion = 2;
+			seventh = true;
+		}
+		else if (romanNum.indexOf("65") != -1)
+		{
+			inversion = 1;
+			seventh = true;
+		}
+		else if (romanNum.indexOf("7") != -1)
+		{
+			inversion = 0;
+			seventh = true;
+		}
+		else if (romanNum.indexOf("64") != -1)
+			inversion = 2;
+		else if (romanNum.indexOf("6") != -1)
+			inversion = 1;
+		else
+			inversion = 0;
+	}
+	/* private int root; //0=tonic, 6=subtonic/leading tone
+	private boolean isMinor; //true=minor root, false=major root
+	private boolean isSeventh; //true if chord is 7th, false if chord is triad
+	private int inversion; //0=none, 1=1st, 2=2nd, 3=3rd
+	 */
+	public int getRoot()
+	{
+		return root;
+	}
+	
+	public boolean isMinor()
+	{
+		return minor;
+	}
+	
+	public boolean isSeventh()
+	{
+		return seventh;
+	}
+	
+	public int getInversion()
+	{
+		return inversion;
+	}
+	
+	public String getFiguredBassInversion()
+	{
+		if (seventh)
+		{
+			if (inversion == 0)
+				return "7";
+			else if (inversion == 1)
+				return "65";
+			else if (inversion == 2)
+				return "43";
+			else
+				return "42";
+		}
+		else
+		{
+			if (inversion == 0)
+				return "";
+			else if (inversion == 1)
+				return "6";
+			else
+				return "64";
+		}
+	}
+	/**
+	 * Identifies the intervals for each scale degree
+	 * array[num] is between num note and (num+1)%7 note
+	 * @return Array of steps for each scale degree starting from tonic note. 
+	 */
+	public int[] getIntervals()
+	{
+		int[] majorIntervals = {2,2,1,2,2,2,1};
+		int[] minorIntervals = {2,1,2,2,1,3,1};
+		
+		if (minor)
+			return minorIntervals;
+		return majorIntervals;
+	}
+	
+	public int getNote(int noteNum)
+	{
+		int note = 0;
+		int[] intervals = getIntervals();
+			
+		for (int i = 0; i < (noteNum % 12); i++)
+		{
+			note = (note + intervals[i]) % 12;
+		}
+		
+		return note;
+	}
+	public int getNote(int noteNum, int tonic)
+	{
+		int note = tonic;
+		int[] intervals = getIntervals();
+			
+		for (int i = 0; i < (noteNum % 12); i++)
+		{
+			note = (note + intervals[i]) % 12;
+		}
+		
+		return note;
+	}
+	
+	public int getBassNote()
+	{
+		int bassNote = getNote(root + inversion * 2);
+		
+		return bassNote;
+	}
+	public int getBassNote(int tonic)
+	{
+		int bassNote = getNote(root + inversion * 2, tonic);
+		
+		return bassNote;
+	}
+	
+	/**
+	 *  returns all notes in an array
+	 *  [0] = root
+	 */
+	public int[] getAllNotes()
+	{
+		int notes[] = new int[(seventh?4:3)];
+		
+		notes[0] = getNote(root);
+		notes[1] = getNote(root + 2);
+		notes[2] = getNote(root + 4);
+		if (seventh)
+			notes[3] = getNote(root + 6);
+		
+		return notes;
+	}
+	public int[] getAllNotes(int tonic)
+	{
+		int notes[] = new int[(seventh?4:3)];
+		
+		notes[0] = getNote(root, tonic);
+		notes[1] = getNote(root + 2, tonic);
+		notes[2] = getNote(root + 4, tonic);
+		if (seventh)
+			notes[3] = getNote(root + 6, tonic);
+		
+		return notes;
+	}
+	/**
+	 *  returns all notes in an array
+	 *  [0] = bass if param is true
+	 */
+	public int[] getAllNotes(boolean startFromBass)
+	{
+		int notes[] = new int[(seventh?4:3)];
+		
+		notes[0] = getNote(root);
+		notes[1] = getNote(root + 2);
+		notes[2] = getNote(root + 4);
+		if (seventh)
+			notes[3] = getNote(root + 6);
+		
+		if (!startFromBass)
+			return notes;
+		
+		int newNotes[] = new int[(seventh?4:3)];
+		
+		newNotes[0] = notes[inversion];
+		newNotes[1] = notes[(inversion + 1) % (seventh?4:3)];
+		newNotes[2] = notes[(inversion + 2) % (seventh?4:3)];
+		if (seventh)
+			newNotes[3] = notes[(inversion + 3) % (seventh?4:3)];
+		
+		return newNotes;
+	}
+	public int[] getAllNotes(boolean startFromBass, int tonic)
+	{
+		int notes[] = new int[(seventh?4:3)];
+		
+		notes[0] = getNote(root, tonic);
+		notes[1] = getNote(root + 2, tonic);
+		notes[2] = getNote(root + 4, tonic);
+		if (seventh)
+			notes[3] = getNote(root + 6, tonic);
+		
+		if (!startFromBass)
+			return notes;
+		
+		int newNotes[] = new int[(seventh?4:3)];
+		
+		newNotes[0] = notes[inversion];
+		newNotes[1] = notes[(inversion + 1) % (seventh?4:3)];
+		newNotes[2] = notes[(inversion + 2) % (seventh?4:3)];
+		if (seventh)
+			newNotes[3] = notes[(inversion + 3) % (seventh?4:3)];
+		
+		return newNotes;
+	}
 	public String toString()
 	{
 		return Arrays.toString(getAllNotes(true));
--- src/SongWriter.java
+++ src/SongWriter.java
@@ -1,14 +1,13 @@
 /**
  * ChordStack object holds info about a specific chord
  *  either increase or decrease the population.
- *  @author Gahwon Lee
+ *  @author Gahwon Lee, Henry Wang
  *  Period: 0
  *  Date: 01-08-16
  */
 
 import java.io.*;
 import java.util.*;
-import javax.sound.midi.*;
 
 public class SongWriter
 {
@@ -18,49 +17,87 @@ public class SongWriter
 	
 	public static void main(String[] args) throws IOException
 	{
-        Scanner fileNameScn = new Scanner(System.in);
-        
-        System.out.print("Txt file name that contains Chord progressions?: ");
-        String fileName = fileNameScn.nextLine();
-        
-        if (fileName.indexOf(".txt") == -1)
-        	fileName += ".txt";
-        
-        Scanner scn = new Scanner(new File(fileName));
-        String keyName = scn.next();
-        String minorKey = scn.next();
-        scn.nextLine();
-        
-        key = new NoteLetter(keyName);
-        isMinor = (minorKey.equals("minor"));
-        
-        time = scn.nextLine();
-        
-        String chordsOneString = scn.nextLine().trim();
-        
-        int chordsAmount = 0;
-        Scanner chordsScnTemp = new Scanner(chordsOneString);
-        
-        while (chordsScnTemp.hasNext())
-        {
-        	chordsAmount++;
-        }
-        
-        //chordsScnTemp.close();
-        
-        Scanner chordsScn = new Scanner(chordsOneString);
-        ChordStack[] chords = new ChordStack[chordsAmount];
-        
-        for (int i = 0; i < chordsAmount; i++)
-        {
-        	chords[i] = new ChordStack(chordsScn.next(), isMinor);
-        }
+		Scanner fileNameScn = new Scanner(System.in);
+		
+		System.out.print("Txt file name that contains Chord progressions?: ");
+		String fileName = fileNameScn.nextLine();
+		
+		if (fileName.indexOf(".txt") == -1)
+			fileName += ".txt";
+		
+		Scanner scn = new Scanner(new File(fileName));
+		String keyName = scn.next();
+		String minorKey = scn.next();
+		scn.nextLine();
+		
+		key = new NoteLetter(keyName);
+		isMinor = (minorKey.equals("minor"));
+		
+		time = scn.nextLine();
+		
+		String chordsOneString = scn.nextLine().trim();
+		
+		int chordsAmount = 0;
+		Scanner chordsScnTemp = new Scanner(chordsOneString);
+		
+		while (chordsScnTemp.hasNext())
+		{
+			chordsAmount++;
+		}
+		
+		//chordsScnTemp.close();
+		
+		Scanner chordsScn = new Scanner(chordsOneString);
+		ChordStack[] chords = new ChordStack[chordsAmount];
+		
+		for (int i = 0; i < chordsAmount; i++)
+		{
+			chords[i] = new ChordStack(chordsScn.next(), isMinor);
+		}
 
-        /*
-        steps:
-        1. add bass notes
+		/*
+		steps:
+		1. analyze chords
+		2. apply resolution rules
+		3. write bass line
+		4. write random soprano line based on probability
+		5. write alto and tenor line based on probability
+		6. check for errors - if errors exist, redo 3-5
+		7. write midi file
 
-         */
-    }
+		 */
+
+	}
+
+	/**
+	 * given an array of stuff and an integer array representing the weight of the stuff, return a thing from the first array based on random weight
+	 * @param stuffs something is chosen randomly from here
+	 * @param weights these are the weights. higher means more chance of getting picked. x<=0 means no chance of getting picked
+	 * @param <T> whatever thing you want to get
+	 * @return randomly picked thing
+	 */
+	public static <T> T calculateProbability(T[] stuffs, int[] weights)
+	{
+		if (stuffs.length != weights.length)
+			throw new IllegalArgumentException("make sure the stuff array and the weight array have the same length");
+
+		int weightSum = 1; //starts with 1 because Math.random does not generate 1
+		for (int weight : weights)
+			weightSum += weight;
+
+		//array of indexes
+		int[] indexes = new int[weightSum];
+		for (int i = 0; i < weights.length; i++)
+		{
+			for (int j = 0; j < weights[i]; j++)
+			{
+				indexes[j] = i;//stuffs[i];
+			}
+		}
+
+		int randomValue = (int) (Math.random() * weightSum);
+
+		return stuffs[indexes[randomValue]];
+	}
 
 }
